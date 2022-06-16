5e5027fd8684dc2feb4a942772f4176fcbc8e779
Gen Taguchi <taguchi@gmail.com>
2022-06-16 10:35:39 +0900

    initial commit

---
+++ main.js
@@ -0,0 +1,23 @@
+'use strict';
+
+{
+  const icons = document.querySelectorAll('.bi');
+
+  icons.forEach(icon => {
+    icon.addEventListener('click', () => {
+      icon.classList.remove('bi-clipboard');
+      icon.classList.add('bi-clipboard-check');
+
+      const listener = (e) => {
+        e.clipboardData.setData('text/plain', icon.dataset.text);
+        e.preventDefault();
+        document.removeEventListener('copy', listener);
+      };
+
+      document.addEventListener('copy', listener);
+      document.execCommand("copy");
+    });
+  });
+  
+}
+
